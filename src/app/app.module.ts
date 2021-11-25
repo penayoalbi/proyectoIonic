95258c3db4e98b9eb6391e33a98b3f48462fa0df
albi.penayo <albi.penayo@gmail.com>
2021-11-25 01:21:05 -0300

    carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ResaltarDirective } from './Directive/resaltar.directive';
+import { FiltrarPipe } from './Pipe/filtrar.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ResaltarDirective, FiltrarPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
